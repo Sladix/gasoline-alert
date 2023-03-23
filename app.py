a319f8b87f5d0986773f00e158f77635d13a4532
Antoine Guillien <sladix@gmail.com>
2023-03-23 09:00:20 +0100

    Remove facet usage to have better precision on when it becomes unavailable

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 yena_file = os.path.join(script_dir, "yena.wav")
 yenapu_file = os.path.join(script_dir, "yenapu.wav")
 
-def get_gas_stations_data(postal_code, gas_type):
+def get_gas_stations_data(postal_code):
     params = {
         "dataset": DATASET,
         "facet": [
@@ -25,7 +25,6 @@ def get_gas_stations_data(postal_code, gas_type):
         ],
         "rows": 1000,
         "refine.cp": postal_code,
-        "refine.carburants_disponibles": gas_type
     }
     response = requests.get(API_URL, params=params)
     return response.json()
@@ -37,13 +36,13 @@ def load_last_known_state(filename):
     last_known_state = {}
     if os.path.exists(filename):
         with open(filename, "r", encoding="utf-8") as csvfile:
-            fieldnames = ["timestamp", "adresse", "availability"]
+            fieldnames = ["timestamp", "adresse", "available"]
             reader = csv.DictReader(csvfile, fieldnames=fieldnames, delimiter=';', lineterminator='\n')
             for row in reader:
                 if row and "adresse" in row:
                     address = row["adresse"]
                     last_known_state[address] = {
-                        "availability": row["availability"] == "True",
+                        "available": row["available"] == "True",
                         "timestamp": row["timestamp"]
                     }
     return last_known_state
@@ -55,9 +54,9 @@ def play_wav_file(filename):
 
 def write_availability_to_file(filename, timestamp, adresse, availability):
     with open(filename, "a", encoding="utf-8") as csvfile:
-        fieldnames = ["timestamp", "adresse", "availability"]
+        fieldnames = ["timestamp", "adresse", "available"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=";", lineterminator='\n')
-        writer.writerow({"timestamp": timestamp, "adresse": adresse, "availability": availability})
+        writer.writerow({"timestamp": timestamp, "adresse": adresse, "available": availability})
 
 def print_confidence_level(date):
     now = datetime.now()
@@ -78,6 +77,28 @@ def print_confidence_level(date):
     
     print(f"{color}{BOLD}{confidence}{RESET}")
 
+def gas_available(record, gas_type):
+    if 'carburants_disponibles' in record['fields']:
+        return gas_type in record['fields']['carburants_disponibles'].split(';')
+    else:
+        return False            
+
+def gas_unavailable(record, gas_type):
+    if 'carburants_indisponibles' in record['fields']:
+        return gas_type in record['fields']['carburants_indisponibles'].split(';')
+    else:
+        return False
+    
+def print_availability(record, gas_type):
+    print_confidence_level(get_timestamp(record['fields'][f'{gas_type.lower()}_maj']))
+    print(f"{gas_type} is available at {record['fields']['adresse']}, {record['fields']['cp']} {record['fields']['ville']} (Updated at: {record['fields'][f'{gas_type.lower()}_maj']})")
+
+def gas_proposed(record, gas_type):
+    return gas_available(record, gas_type) or gas_unavailable(record, gas_type)
+
+def get_timestamp(date):
+    return datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+
 def main(postal_code=None, gas_type=None, time_sleep=None):
     valid_gas_types = ["E10", "SP98", "SP95", "E85", "Gazole", "GPLc"]
     if gas_type not in valid_gas_types:
@@ -94,7 +115,7 @@ def main(postal_code=None, gas_type=None, time_sleep=None):
     last_known_state = load_last_known_state(filename)
     first_run = True
     while True:
-        data = get_gas_stations_data(postal_code, gas_type)
+        data = get_gas_stations_data(postal_code)
         records = data["records"]
 
         current_state = {}
@@ -106,35 +127,28 @@ def main(postal_code=None, gas_type=None, time_sleep=None):
         has_unavailable = False
 
         for record in records:
+            if (not gas_proposed(record, gas_type)):
+                continue
+
             address = record["fields"]["adresse"]
-            availability = gas_type in record["fields"]["carburants_disponibles"].split(";")
+            availability = gas_available(record, gas_type)
 
             if first_run and availability:
-                update_time = datetime.strptime(record["fields"][f"{gas_type.lower()}_maj"], "%Y-%m-%d %H:%M:%S")
+                update_time = get_timestamp(record["fields"][f"{gas_type.lower()}_maj"])
                 if latest_update is None or update_time > latest_update:
                     latest_update = update_time
                     latest_station = record
 
             current_state[address] = availability
 
-            if address not in last_known_state or last_known_state[address]["availability"] != availability:
+            if address not in last_known_state or current_state[address] != last_known_state[address]['available']:
+                write_availability_to_file(filename, update_time if availability else datetime.strptime(record['record_timestamp'], "%Y-%m-%dT%H:%M:%S.%fZ"), address, availability)
+                changed_count+= 1
                 if availability:
-                    timestamp = record["fields"][f"{gas_type.lower()}_maj"]
-                    print_confidence_level(datetime.strptime(timestamp))
-                    print(f"{timestamp} : ALERT : {gas_type} is available at {record['fields']['adresse']}, {record['fields']['cp']} {record['fields']['ville']}")
+                    print_availability(record, gas_type)
                     has_available = True
                 else:
-                    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                     has_unavailable = True
-                write_availability_to_file(filename, timestamp, address, availability)
-                changed_count += 1
-
-        for address in last_known_state:
-            if address not in current_state and last_known_state[address]["availability"] != False:
-                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                write_availability_to_file(filename, timestamp, address, False)
-                has_unavailable = True
-                changed_count += 1
 
         if changed_count > 0:
             print(f"{changed_count} stations with changed availability.")
@@ -143,11 +157,11 @@ def main(postal_code=None, gas_type=None, time_sleep=None):
         elif has_unavailable:
             play_wav_file(yenapu_file)
 
-        last_known_state = {address: {"availability": availability} for address, availability in current_state.items()}
+        last_known_state = {address: {"available": availability} for address, availability in current_state.items()}
 
         if first_run and latest_station is not None:
-            print_confidence_level(latest_update)
-            print(f"{gas_type} is available at {latest_station['fields']['adresse']}, {latest_station['fields']['cp']} {latest_station['fields']['ville']} (Updated at: {latest_station['fields'][f'{gas_type.lower()}_maj']})")
+            print("--- BEST AVAILABILITY RIGHT NOW ---")
+            print_availability(latest_station, gas_type)
 
         first_run = False
 
